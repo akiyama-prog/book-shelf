08c207714b67c4b9046b4bcf68f22350c8fb4fa2
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2020-10-02 19:37:04 +0900

    laravelと連携

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        {{ data }}
+      </div>
+
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+  import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+  export default {
+    async asyncData({ app }) {
+      const data = await app.$axios.$get('http://localhost:8000/api')
+      return {
+        data
+      }
+    }
+  }
+</script>
